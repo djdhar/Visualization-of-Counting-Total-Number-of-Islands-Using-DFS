69294c214501c5e22c487b687962ee578c11c2cf
DIBYAJYOTI DHAR <42883382+djdhar@users.noreply.github.com>
2020-04-23 21:04:20 +0530

    Add files via upload

---
+++ Number Of Island/mainwindow.h
@@ -0,0 +1,64 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtGui>
+#include <QtCore>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public slots:
+    void Mouse_Pressed();
+    void showMousePosition(QPoint& pos);
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    QImage sourceImage;
+
+private slots:
+
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    bool RatInAMaze(int i, int j);
+
+
+
+
+    void on_pushButton_3_clicked();
+    void PrintVectors();
+    void insertionSort();
+    void mergeSort(int,int);
+    void merge(int,int,int);
+    void quickSort(int,int);
+    int partition(int,int);
+    void heapSort(int);
+    void heapify(int,int);
+    bool RatInAMazeHashMap(int x, int y);
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_6_clicked();
+    void dfs(int i,int j, QColor color);
+
+protected:
+
+
+private:
+    Ui::MainWindow *ui;
+    QPoint p1,p2;
+    void point(QImage&,int,int,int, int, int, int);
+    void ClearScreen(void);
+    void SpeacialClearance(void);
+
+};
+
+#endif // MAINWINDOW_H
